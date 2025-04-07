0a12ed32d059b82af0f6b2133761e0a15d778d8e
Harpal Rajput <harpalsinghrajput007@gmail.com>
2025-04-07 18:51:28 +0530

    basics, variable and data types

---
+++ intro.py
@@ -0,0 +1,9 @@
+print("hello world!")
+print(5+3)
+
+print("Prints the statement") #prints the statement or any other things
+print("Prints the statement \n Escape the sequence") #escape sequence
+
+print(" 'hello' ")
+# print("hello",6,7,8,sep=~,end=007\n)
+
---
+++ variableanddatatypes.py
@@ -0,0 +1,28 @@
+a = 5
+b = complex(8,2) #numeric
+print(b)
+c = 8.5
+
+d = "harpal" #string
+
+e = True #boolean
+
+l = [4,5,6,7,["ram, krushna"],[34,45]] #sequenced 
+t = (3,4,2,3,("parrot, bulbul"))
+
+dict = { #mapped
+    "name": "harpal",
+    "age": 25,
+    "city": "delhi",
+}
+
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(l))
+print(type(t))
+print(type(dict))
