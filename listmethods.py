3fd914c9da81fcdb74107e937a9dab4303b17db7
Harpal Rajput <harpalsinghrajput007@gmail.com>
2025-04-09 23:00:40 +0530

    Learnt about list methods : sort, reverse, index, count, copy, append, insert, extend and concat

---
+++ listmethods.py
@@ -0,0 +1,45 @@
+#1. list.sort()
+li = [2,1,3,2,4,7,5,6,9,8]
+# li.sort()
+# print(li)
+
+# li.sort(reverse=True)
+# print(li)
+
+
+#2.reverse()
+# li.reverse()
+# print(li)
+
+
+#3.index()
+print(li.index(7))
+
+
+#4.count()
+print(li.count(2))
+
+
+#5.copy()
+li2 = li.copy()
+# print(li2)
+
+
+#6.append()
+# li.append(10)
+# print(li)
+
+
+#7.insert()
+li.insert(1,5)
+print(li)
+
+
+#8.extend()
+li.extend(li2)
+print(li)
+
+
+#9.concatenating()
+li3 = li + li2
+print(li3)
