22e630e4746751044a3a578580dcf7f2c1c39579
Harpal Rajput <harpalsinghrajput007@gmail.com>
2025-04-14 15:12:55 +0530

    Learnt about sets in python

---
+++ sets.py
@@ -0,0 +1,15 @@
+info = {"Harpal", 19, 89.50, True, 19} #returns only distinct values here it will return 19 only one time
+print(info)
+
+
+se = {}
+s = set()
+print(s)
+print(type(se)) #dict
+print(type(s)) #set
+
+
+
+#accessing set items
+for i in info:
+    print(i) #returns all values in the set
