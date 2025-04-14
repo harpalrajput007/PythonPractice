6573a84ffded858898294eb7554c1ef904d0dd85
Harpal Rajput <harpalsinghrajput007@gmail.com>
2025-04-14 23:24:45 +0530

    Learnt how to use else keyword with for and while loop

---
+++ forloopwithelse.py
@@ -0,0 +1,16 @@
+for x in range(5):
+    print(f"currently on {x+1}!")
+    
+else:
+    print("we are done with the loop")
+print("out of loop")
+
+
+
+i = 0;
+while i < 5:
+    print(i)
+    i += 1
+    
+else:
+    print("we are done with the loop")
