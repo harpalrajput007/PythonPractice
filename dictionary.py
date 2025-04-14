b4840cb0a4f6de83e7e8d7b0bdb32f91faf29197
Harpal Rajput <harpalsinghrajput007@gmail.com>
2025-04-14 22:42:51 +0530

    Learnt about dictionary also learnt about how to access dictionary items

---
+++ dictionary.py
@@ -0,0 +1,26 @@
+info = {
+    'name' : 'karan',
+    'age' : 25,
+    'city' : 'delhi',
+}
+print(info)
+
+
+
+#Accessing dictionary items
+print(info['name'])
+
+
+
+#Accessing multiple values
+print(info.values())
+
+
+
+#Accessing multiple keys
+print(info.keys())
+
+
+
+#Accessing key - values pairs
+print(info.items())
