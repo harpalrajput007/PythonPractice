961be32977a624148a6487752cf907c18879bbad
Harpal Rajput <harpalsinghrajput007@gmail.com>
2025-04-14 23:19:21 +0530

    Learnt about various dictionary methods

---
+++ dictionarymethods.py
@@ -0,0 +1,22 @@
+#1. update()
+stu_marks1 = {1: 98, 2: 97, 3: 96}
+stu_marks2 = {4: 95, 5: 97, 6: 96}
+# stu_marks1.update(stu_marks2)
+# print(stu_marks1)
+
+
+#2. clear()
+# stu_marks1.clear()
+# print(stu_marks1)  # Output: {}
+
+#3. pop()
+# stu_marks1.pop(1)
+# print(stu_marks1)  
+
+#4. popitem()
+# stu_marks1.popitem()
+# print(stu_marks1)
+
+#5. del
+del stu_marks1[1]
+print(stu_marks1)  
